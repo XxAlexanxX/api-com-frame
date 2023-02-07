01af683fba84b188a8ac5347536dbefc46168b9d
Pablo Rodrigues Alexandre Silva <pablo0104@yahoo.com.br>
2023-02-07 09:47:00 -0300

    api framework

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // config inicial
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
+
+const Person = require('./models/Person')
 
 // forma de ler JSON / middlewares
 app.use(
@@ -19,5 +22,21 @@ app.get('/', (req, res) => {
     res.json({ message: 'Oi  Express!' })    
 })
 
+// mongodb+srv://PabloRodrigues:syO8Oz7JwGV4LX8r@apicluster.lnnlhv7.mongodb.net/bancodaapi?retryWrites=true&w=majority
+
 // entregar uma porta
-app.listen(3000)
+const DB_USER = 'PabloRodrigues'
+const DB_PASSWORD = encodeURIComponent('syO8Oz7JwGV4LX8r')
+
+mongoose
+.connect(
+    `mongodb+srv://${DB_USER}:${DB_PASSWORD}@apicluster.lnnlhv7.mongodb.net/?retryWrites=true&w=majority`,
+    )
+.then(() => {
+    console.log("Conectamos ao MongoDB!")
+    app.listen(3000)
+
+})
+.catch((err) => console.log(err))
+
+
